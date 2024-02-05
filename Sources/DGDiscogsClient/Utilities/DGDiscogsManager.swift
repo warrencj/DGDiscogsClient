4cc8a92327be91d3fedad8140a510caff3e2eae9
Chris Warren <christopher.j.warren@gmail.com>
2024-02-05 11:37:01 -0700

    convert to session

--- Sources/DGDiscogsClient/Utilities/DGDiscogsManager.swift
+++ Sources/DGDiscogsClient/Utilities/DGDiscogsManager.swift
@@ -12,46 +12,46 @@ import Alamofire
 import SwiftyJSON
 
 public class DGDiscogsManager {
-    
-    public static let sharedInstance = DGDiscogsManager()
-    
-    public var user: DGDiscogsUser! = nil
-    
-		public var session = Session.default
-    
-    public func getAuthenticatedUser(
-        setAsMainUser: Bool = false,
-        completion : @escaping DGDiscogsCompletionHandlers.userAuthenticationCompletionHandler) {
-        
-        let url: URLConvertible = "oauth/identity"
-        
-        print("Must get auth user")
-        
-        RequestHelper.sharedInstance.request(
-            url: url,
-            method: .get,
-            parameters: nil,
-            
-            completion: { (response, json, error) in
-                
-                if let error = error {
-                    completion(.failure(error: error))
-                    return
-                }
-                
-                guard let json = json else {
-                    completion(.failure(error: NSError(domain: "DGDiscogsClient", code: 500, userInfo: nil)))
-                    return
-                }
-                
-                let user = DGDiscogsUser(json: json)
-                
-                if setAsMainUser {
-                    self.user = user
-                }
-                
-                completion(.success(user: user, jsonString: json.rawString()))
-        })
-    }
-    
+	
+	public static let sharedInstance = DGDiscogsManager()
+	
+	public var user: DGDiscogsUser! = nil
+	
+	public var session = Session.default
+	
+	public func getAuthenticatedUser(
+		setAsMainUser: Bool = false,
+		completion : @escaping DGDiscogsCompletionHandlers.userAuthenticationCompletionHandler) {
+			
+			let url: URLConvertible = "oauth/identity"
+			
+			print("Must get auth user")
+			RequestHelper.sharedInstance.session = self.session
+			RequestHelper.sharedInstance.request(
+				url: url,
+				method: .get,
+				parameters: nil,
+				
+				completion: { (response, json, error) in
+					
+					if let error = error {
+						completion(.failure(error: error))
+						return
+					}
+					
+					guard let json = json else {
+						completion(.failure(error: NSError(domain: "DGDiscogsClient", code: 500, userInfo: nil)))
+						return
+					}
+					
+					let user = DGDiscogsUser(json: json)
+					
+					if setAsMainUser {
+						self.user = user
+					}
+					
+					completion(.success(user: user, jsonString: json.rawString()))
+				})
+		}
+	
 }
